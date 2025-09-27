230a3429492332a62acf57ab40c054a30eff4155
Franco di Napoli <franco.dinapoli@tiendanube.com>
2025-09-27 12:03:50 -0300

    Health controller and endpoint

---
+++ src/main/java/ar/edu/utn/dds/k3003/controller/HealthController.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.dds.k3003.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/health")
+  public ResponseEntity<Map<String, String>> health() {
+    return ResponseEntity.ok(Map.of("status", "UP"));
+  }
+}
+
+
